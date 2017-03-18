981c435cd3688b8621d8bd5ebb9761e6f7c6d895
Yannick Francois <yaf@elsif.fr>
2017-03-18 23:29:25 +0100

    Affiche un titre des journaux dejà saisir

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
 
-app.use(express.static('public'))
+app.use(express.static('public'));
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for pars
+app.set('view engine', 'pug')
+
+var journaux = []
+
+app.get("/", function (request, response) {
+  response.render('index', { journaux: journaux }, function(err, html) {
+    console.log(err)
+    response.send(html);
+  });
+});
+
+app.post("/journal", function (request, response) {
+  var journal = {content: request.body, title: "something"}
+  journaux.push(journal);
+  response.redirect("/");
+});
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
-})
+});
 
 
