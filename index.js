b89e8da9d4baae9f1b5a8acae19a029829e05fcb
Yannick Francois <yaf@elsif.fr>
2017-03-30 15:57:23 +0200

    une page pour ajouter un journal

--- index.js
+++ index.js
@@ -7,12 +7,21 @@ app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for pars
 app.set('view engine', 'pug')
 
+
 var journaux = []
 
 app.get("/", function (request, response) {
   response.render('index', { journaux: journaux }, function(err, html) {
     console.log(err)
-    response.send(html);
+      response.send(html);
+  });
+});
+
+app.get("/ajouter-journal", function (request, response) {
+  console.log('dans ajouter journal');
+  response.render('ajouter-journal', {}, function(err, html) {
+    console.log(err)
+      response.send(html);
   });
 });
 
