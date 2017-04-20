14ab74d6187a8d759aeb2b4684ac473fc5faf7fb
Yannick Francois <yaf@elsif.fr>
2017-04-20 22:48:55 +0200

    Ajout d'un linter

--- index.js
+++ index.js
@@ -1,49 +1,43 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var J = require('./lib/journalizer');
+var express = require("express");
+var bodyParser = require("body-parser");
+var J = require("./lib/journalizer");
 var app = express();
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.set('view engine', 'pug')
+app.set("view engine", "pug");
 
-
-var journaux = []
+var journaux = [];
 
 app.get("/", function (request, response) {
-  response.render('index', { journaux: J.journal_list() }, function(err, html) {
-    if (err) { console.log(err); }
+  response.render("index", { journaux: J.journal_list() }, function (err, html) {
+    if (err) { response.send(err); }
     response.send(html);
   });
 });
 
 app.get("/ajouter-journal", function (request, response) {
-  response.render('ajouter-journal', {}, function(err, html) {
-    if (err) { console.log(err); }
+  response.render("ajouter-journal", {}, function (err, html) {
+    if (err) { response.send(err); }
     response.send(html);
   });
 });
 
-app.get('/:slug', function (request, response) {
+app.get("/:slug", function (request, response) {
   response.send("journal demandé : " + request.params.slug);
 });
-
 
 app.post("/journal", function (request, response) {
   var journal = J.create_journal({
     author: request.body.author,
     date: request.body.date,
     activity: request.body.activity,
-    content: request.body.learn})
+    content: request.body.learn});
 
   journaux.push(journal);
 
   response.redirect("/");
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
-});
-
-
+app.listen(3000, function () { });
